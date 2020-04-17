254fbbefcb8256c1dd6e920d0be175c482f64651
Morgan Funtowicz <funtowiczmo@gmail.com>
2020-04-17 12:52:43 +0200

    Added unittest for Flax models.

--- tests/utils.py
+++ tests/utils.py
@@ -2,8 +2,7 @@ import os
 import unittest
 from distutils.util import strtobool
 
-from transformers.file_utils import _tf_available, _torch_available
-
+from transformers.file_utils import _tf_available, _torch_available, _flax_available
 
 SMALL_MODEL_IDENTIFIER = "julien-c/bert-xsmall-dummy"
 DUMMY_UNKWOWN_IDENTIFIER = "julien-c/dummy-unknown"
@@ -91,6 +90,18 @@ def require_tf(test_case):
     """
     if not _tf_available:
         test_case = unittest.skip("test requires TensorFlow")(test_case)
+    return test_case
+
+
+def require_flax(test_case):
+    """
+    Decorator marking a test that requires JAX & Flax
+
+    These tests are skipped when one / both are not installed
+
+    """
+    if not _flax_available:
+        test_case = unittest.skip("test requires JAX & Flax")(test_case)
     return test_case
 
 
